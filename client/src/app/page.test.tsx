a0087315266945a5ebe506266ade4ac600ae6638
Bo <hungdang.220405@gmail.com>
2026-05-22 22:31:37 +0700

    fix a little thing about page test on frontend site

--- client/src/app/page.test.tsx
+++ client/src/app/page.test.tsx
@@ -5,7 +5,7 @@ describe('Home page', () => {
   it('renders main heading', () => {
     render(<Home />)
     expect(
-      screen.getByRole('heading', { name: /Supply Chain Manager/i }),
+      screen.getByRole('heading', { name: /VeloChain Supply Chain/i }),
     ).toBeInTheDocument()
   })
 
@@ -13,7 +13,8 @@ describe('Home page', () => {
     render(<Home />)
     expect(screen.getByText(/Register Roles/i)).toBeInTheDocument()
     expect(screen.getByText(/Order Materials/i)).toBeInTheDocument()
-    expect(screen.getByText(/Track Materials/i)).toBeInTheDocument()
-    expect(screen.getByText(/Supply Materials/i)).toBeInTheDocument()
+    expect(screen.getByText(/Control Chain/i)).toBeInTheDocument()
+    expect(screen.getByText(/Track Item/i)).toBeInTheDocument()
   })
 })
+
